d5207b938812ae57e74167042afa60a0dcd8c0cc
Tom Elliott <tom@letsdothis.com>
2023-05-25 14:33:32 +0100

    Make it look a bit prettier

--- app/form.tsx
+++ app/form.tsx
@@ -1,20 +1,31 @@
 'use client';
 
 import { useCallback, useEffect, useRef, useState } from 'react';
+import {
+  ChatBubbleLeftRightIcon,
+  SparklesIcon
+} from '@heroicons/react/24/solid';
 
 import { queryModelRequest } from 'lib/client/api';
 
+type QuestionAndAnswer = {
+  question: string;
+  answer: string;
+};
+
 export const Form = () => {
   const [query, setQuery] = useState('');
-  const [response, setResponse] = useState('');
+  const [responses, setResponses] = useState<QuestionAndAnswer[]>([]);
 
   const textareaRef = useRef<HTMLTextAreaElement>(null);
   const textareaElement = textareaRef.current;
 
   const sendQuery = useCallback(async () => {
-    const response = await queryModelRequest(query);
-    setResponse(response.answer);
-  }, [query]);
+    const apiResponse = await queryModelRequest(query);
+    const qAndA = { question: query, answer: apiResponse.answer };
+    const newResponses = [...responses, qAndA];
+    setResponses(newResponses);
+  }, [responses, query]);
 
   useEffect(() => {
     const listener = (event: KeyboardEvent) => {
@@ -39,20 +50,28 @@ export const Form = () => {
     <>
       <main className="p-4 md:p-10 mx-auto max-w-7xl h-full">
         <div className="col-span-full">
-          <label
-            htmlFor="response"
-            className="block text-sm font-medium leading-6 text-gray-900"
-          >
-            Response:
-          </label>
-          <div className="mt-2">
-            <div
-              id="response"
-              className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-            >
-              {response}
+          {responses.map((qAndA, idx) => (
+            <div className="mt-2 mb-4" key="idx">
+              <div
+                id={`question-${idx}`}
+                className="block w-full border-0 mr-16 mb-1"
+              >
+                <ChatBubbleLeftRightIcon className="h-6 w-6 mr-2 text-gray-900 inline-block" />
+                <span className="inline-block rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 bg-white p-4">
+                  {qAndA.question}
+                </span>
+              </div>
+              <div
+                id={`response-${idx}`}
+                className="block w-full border-0 ml-16 mb-4"
+              >
+                <span className="inline-block rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 bg-gray-50 p-4">
+                  {qAndA.answer}
+                </span>
+                <SparklesIcon className="h-6 w-6 ml-2 text-blue-500 inline-block" />
+              </div>
             </div>
-          </div>
+          ))}
         </div>
       </main>
 
@@ -64,7 +83,7 @@ export const Form = () => {
             ref={textareaRef}
             placeholder="Send a message"
             rows={1}
-            className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 p-4"
             defaultValue={query}
           />
           {/* <button
